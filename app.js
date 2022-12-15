985954d5a1555d42810f44334a6595e7e3e58e62
rizkysadar <47404986+rizkysadar@users.noreply.github.com>
2022-12-15 17:34:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('hello my name is rizky satria d\n')
+    res.send('hello my name is rizky satria darmawan\n')
 })
 
 app.listen(PORT, HOST)
